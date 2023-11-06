404b142d12fed4c907e4c6281c82932cf83c4114
mexicola84 <i@jan-grimm.com>
2023-11-06 17:00:22 +0100

    Finished composing compelx interfaces

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,9 +8,27 @@ A view showing the list of landmarks.
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        LandmarkList()
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
     }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label ("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
 }
 
 #Preview {
