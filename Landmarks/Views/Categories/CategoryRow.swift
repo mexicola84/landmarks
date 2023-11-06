5c235b73d9759abacac8df50844940e8d4c40507
mexicola84 <i@jan-grimm.com>
2023-11-06 16:59:09 +0100

    Added category view and included this in content view with new tab navigation.

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Jan Grimm on 06.11.23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
