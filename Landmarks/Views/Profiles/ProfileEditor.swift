31e594ac26799ca569403c0a8bc8742b4b83d165
mexicola84 <i@jan-grimm.com>
2023-11-07 08:09:20 +0100

    Added „edit“ button to ProfileHost as well as profile to ModelData. Defined profile editor by implementing ProfileEditor view. ProfileEditor edited Texts, buttons and picker for seasonal photos as well as a datePicker.

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Jan Grimm on 07.11.23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
