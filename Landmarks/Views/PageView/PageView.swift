6846f005e629fad4155ae67cab6417714d43feb7
mexicola84 <i@jan-grimm.com>
2023-11-12 21:23:05 +0100

    Implemented a plage control for featured landmark navigation. Also implemented UIKit view in SwiftUI view with UIViewRepresentable in PageControl.swift.

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-        PageViewController(pages: pages)
+        // $ um das Keyword zum @State zu linken.
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+
+//  Experiement
+//      Button(action: {currentPage += 1}, label: {
+//          Text("Skip")
+//      })
     }
 }
 
