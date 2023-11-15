97de27cd94d3ea64c5577946ea46b931802050ee
mexicola84 <i@jan-grimm.com>
2023-11-15 23:12:23 +0100

    Updated the row view and list view for watchOS, iOs and macOS. Added built-in menu command and shortcut and a custom menu command.

---
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Jan Grimm on 15.11.23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkList()
+        .environment(ModelData())
+}
